7854106896166ac87ed1e79fde3472fbccd4ab38
SanDiskPL <98775072+SanDiskPL@users.noreply.github.com>
2022-01-31 22:40:06 +0100

    Branch test

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int main() {
     int suma;
 
     printf("Narzedzia programistyczne program\n");
-    printf("Podaj slowo jakie chcesz wyswietlic:");
+    printf("Podaj slowo jakie chcesz wyswietlic:")
     scanf("%s", &slowo);
     printf("Slowo to: %s", slowo);
 
-    printf("\nPodaj dwie liczby, ktore chcesz dodac:");
+    printf("\nPodaj dwie liczby, ktore chcesz dodac:")
     scanf("%d %d", &liczba1, &liczba2);
-    suma = liczba1 + liczba2;
-    printf("%d + %d = %d", liczba1, liczba2, suma);
+    suma = liczba1 + test2;
+    printf("%d + %d = %d", test1, liczba2, suma)
 
     printf("\nNarzedzia programistyczne");
-    return 0;
+    test
 
 }
