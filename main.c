d44e8524a651abb8d83746a1d72fa26c8d1a7d39
SanDiskPL <98775072+SanDiskPL@users.noreply.github.com>
2022-01-31 21:55:36 +0100

    Dodanie opcji dodawania w nowym branchu

--- main.c
+++ main.c
@@ -2,12 +2,18 @@
 
 int main() {
     char slowo[25];
+    int liczba1, liczba2;
+    int suma;
 
     printf("Narzedzia programistyczne program\n");
     printf("Podaj slowo jakie chcesz wyswietlic:");
     scanf("%s", &slowo);
     printf("Slowo to: %s", slowo);
 
+    printf("\nPodaj dwie liczby, ktore chcesz dodac:");
+    scanf("%d %d", &liczba1, &liczba2);
+    suma = liczba1 + liczba2;
+    printf("%d + %d = %d", liczba1, liczba2, suma);
     return 0;
 
 }
